019bf4b611e09aee01b8506086e1cb0ca5951386
Rishabh Kumar Jain <rishjain@adobe.com>
2022-12-19 10:08:13 +0530

    add waka time plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -100,6 +100,9 @@ return packer.startup(function(use)
   -- Git Integration
   use 'tpope/vim-fugitive'
 
+  -- Analytics
+  use 'wakatime/vim-wakatime'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
